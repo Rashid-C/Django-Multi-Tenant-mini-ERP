131c6ae435916d3a1200e36e2ead4d2e614c5e63
Rashid-C <anurashid11055@gmail.com>
2026-06-08 14:39:31 +0530

    Base of ERP in Tenant

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,7 +2,9 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        modal=Product
-        fields=['id','name','sku','price']
+        model = Product
+        fields = ['id', 'name', 'sku', 'price']
--- tenants/models.py
+++ tenants/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 
-
 class Tenant(models.Model):
-    name=models.CharField(max_length=255)
-    subdommain=models.CharField(max_length=100, unique=True)
-    is_active=models.BooleanField(default=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    
+    name = models.CharField(max_length=255)
+    subdomain = models.CharField(max_length=100, unique=True)  
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
-    
-    
-    # Abstract base model that other apps will inherit
+
 class TenantAwareModel(models.Model):
-    tenant=models.ForeignKey(Tenant, on_delete=models.CASCADE)
-    
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
     class Meta:
-        abstract=True
-        # Tells Django not to create a separate table for this model
+        abstract = True

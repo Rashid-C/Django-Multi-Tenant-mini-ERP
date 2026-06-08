96afd90afa275472bbf6b1c47c932bd008d8b20b
Rashid-C <anurashid11055@gmail.com>
2026-06-08 14:47:14 +0530

    Connect User Models to Tenants

--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,5 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
 class Tenant(models.Model):
@@ -14,3 +16,11 @@ class TenantAwareModel(models.Model):
 
     class Meta:
         abstract = True
+        
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.tenant.name}"

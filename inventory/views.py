000a99e9ab4a5cad6ca5f479d7af897059e17f9d
Rashid-C <anurashid11055@gmail.com>
2026-06-08 12:10:41 +0530

    Viewset

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+
+    # Force READ operations to only fetch data belonging to the authenticated tenant
+    def get_queryset(self):
+        tenant = self.request.tenant
+        return Product.objects.filter(tenant=tenant)
+
+    # Automatically attach the tenant context on WRITE operations
+    def perform_create(self, serializer):
+        serializer.save(tenant=self.request.tenant)
